6b1854954a18f2526bc3a18fa6951a4dbb3ae59e
tessoka <akos.gollitz@gmail.com>
2022-02-01 14:37:56 +0100

    sidebar finalized

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -6,13 +6,26 @@ const Home = () => {
   
 
   return (
-    <div className='container container-home'>
+    <>
+    <div className="logo-container">
+      <div className="the-box">
+
+      </div>
+      <div className="text-box">
+        <p>The</p>
+        <p>RocketLab</p>
+        <p>Godspeed to you!</p>
+      </div>
+    </div>
+
+    <div className="container container-home">
       <h2>Welcome to the ToDo App!</h2>
       <div className="home-btn-box">
         <Link to="/registration"><button className="btn btn-cta">Sign Up!</button></Link>
         <Link to="/login"><button className="btn btn-cta">Login</button></Link>
       </div>
     </div>
+    </>
   )
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,54 +1,76 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { ThemeContext } from "../utility/Context"
 import { Link } from 'react-router-dom'
-import {PlanetLogo, LogoutLogo, StatisticLogo, SettingsLogo, ProfileLogo, MsgLogo, MenuLogo, DashboardLogo} from '../svg/svg-d-texts'
+import {RocketLogo, PlanetLogo, LogoutLogo, StatisticLogo, SettingsLogo, ProfileLogo, MsgLogo, MenuLogo, DashboardLogo} from '../svg/svg-d-texts'
 
 const Navbar = () => {
+
+  const [ isNavOpen, setIsNavOpen ] = useState(true)
+
+  const handleClickBurger = () => {
+    setIsNavOpen(!isNavOpen)
+  }
+
   return (
-    <nav>
+    <nav className={isNavOpen ? "nav-big" : "nav-small"}>
       <div className="navbar-top">
-        <div className="logo-box">
-          <svg className="icon icon-planet" viewBox="0 0 512 512"><path d={PlanetLogo} /></svg>
-          <p>Planet T</p>
+        <div className={isNavOpen ? "logo-box" : "logo-box not-visible"}>
+          <svg className="icon icon-planet" viewBox="0 0 512 512"><path d={RocketLogo} /></svg>
+          <p>
+            <p className="small-the">The</p>
+            RocketLab
+          </p>
         </div>
-        <div className="hamburger-box">
+        <div className="navbar-top-bot-btn-box" onClick={handleClickBurger}>
           <svg svg className="icon icon-hamburger" viewBox="0 0 512 512"><path stroke="currentColor" strokeLinecap="round" strokeMiterlimit="10" strokeWidth="48" d={MenuLogo} /></svg>
         </div>
       </div>
       <ul>
+      <li>
+          <div className="icon-box">
+            <svg className="icon" viewBox="0 0 512 512"><path d={PlanetLogo} /></svg>
+          </div>
+          <p className={isNavOpen ? "" : "not-visible"}>Home</p>
+          { !isNavOpen && <span>Home</span> }
+        </li>
         <li>
           <div className="icon-box">
             <svg className="icon" viewBox="0 0 512 512"><path d={DashboardLogo} /></svg>
           </div>
-          <p>Dashboard</p>
+          <p className={isNavOpen ? "" : "not-visible"}>Dashboard</p>
+          { !isNavOpen && <span>Dashboard</span> }
         </li>
         <li>
           <div className="icon-box">
             <svg className="icon" viewBox="0 0 512 512"><path d={ProfileLogo} /></svg>
           </div>
-          <p>Profile</p>
+          <p className={isNavOpen ? "" : "not-visible"}>Profile</p>
+          { !isNavOpen && <span>Profile</span> }
         </li>
         <li>
           <div className="icon-box">
             <svg className="icon" viewBox="0 0 512 512"  strokeLinejoin="round"><path d={MsgLogo} /></svg>
           </div>
-          <p>Messages</p>
+          <p className={isNavOpen ? "" : "not-visible"}>Messages</p>
+          { !isNavOpen && <span>Messages</span> }
         </li>
         <li>
           <div className="icon-box">
             <svg className="icon" viewBox="0 0 512 512"><path d={StatisticLogo} /></svg>
           </div>
-          <p>Analytics</p>
+          <p className={isNavOpen ? "" : "not-visible"}>Analytics</p>
+          { !isNavOpen && <span>Analytics</span> }
         </li>
         <li>
           <div className="icon-box">
             <svg className="icon" viewBox="0 0 512 512"><path d={SettingsLogo} /></svg>
           </div>
-          <p>Settings</p>
+          <p className={isNavOpen ? "" : "not-visible"}>Settings</p>
+          { !isNavOpen && <span>Settings</span> }
         </li>
       </ul>
       <div className="navbar-bot">
-        <div className="user-box">
+        <div className={isNavOpen ? "user-box" : "user-box not-visible"}>
           <div className="profile-pic">
 
           </div>
@@ -61,8 +83,9 @@ const Navbar = () => {
             </p>
           </div>
         </div>
-        <div className="log-box">
-        <svg className="icon" viewBox="0 0 512 512"><path d={LogoutLogo} /></svg>
+        <div className="navbar-top-bot-btn-box">
+          <svg className="icon" viewBox="0 0 512 512"><path d={LogoutLogo} /></svg>
+          <span>Logout</span>
         </div>
       </div>
     </nav>
